4ad3bb56df3eb0eec382668635626c3dd3ade80d
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-07-27 11:15:27 +0200

    getElementById & querySelector in game commit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 import styles from './style.css'
 
-console.log('App working!')
+const form = document.getElementById('form')
+const input = form.querySelector('#questionInput')
+const button = form.querySelector('#questionSubmit')
+
+console.log('form',form)
+console.log('input',input)
+console.log('button',button)
