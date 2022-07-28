c8e329c674bd3ee516947f3bbf6cb4cd32d90af2
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-07-28 17:09:05 +0300

    base functionality is done commit

--- src/question.js
+++ src/question.js
@@ -20,12 +20,12 @@ export class Question {
 
     static fetch(token) {
         if (!token) {
-            return Promise.resolve(`<p class="error">You haven't token, sorry bro</p>`)
+            return Promise.resolve(`<p class="error">You haven't token, sorry bro =(</p>`)
         }
         return fetch(`https://questions-app-e3de4-default-rtdb.europe-west1.firebasedatabase.app/question.json?auth=${token}`)
             .then(res => res.json())
             .then(res => {
-                if (res.error) {
+                if (res && res.error) {
                     return `<p class="error">${res.error}</p>`
                 }
                 return res
@@ -50,7 +50,7 @@ export class Question {
     static questionsToHTML(questions) {
         return questions.length
             ? `<ol>${questions.map(q => `<li>${q.text}</li>`).join('')}</ol>`
-            : `<p>You haven't any questions right now</p>`
+            : `<p class="haventquestions">You haven't any questions right now =)</p>`
     }
 }
 
